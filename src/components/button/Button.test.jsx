03d2f140a5c228d7d8afa35c5fd10dc7ce5781e6
yanet <yanettr2024@gmail.com>
2023-09-11 18:47:10 -0500

    Elimar componente footer

---
+++ src/components/button/Button.test.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { Button } from '../../components/button/Button';
+
+import { describe, test, jest, expect } from '@jest/globals';
+
+describe('Button', () => {
+    test('should call onClick when button is clicked', () => {
+        // Mock the onClick function
+        const onClickMock = jest.fn();
+
+        // Render the component
+        const { getByText } = render(
+          <Button onClick={onClickMock} text="Ingresar" />
+        );
+
+        // Find the button element and simulate a click event
+        const button = getByText('Ingresar');
+        fireEvent.click(button);
+
+        // Assert that onClick has been called
+        expect(onClickMock).toHaveBeenCalledTimes(1);
+      });
+})
